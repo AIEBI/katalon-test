2e2c2d16fb6fcb8a327b4539035711f4f85d676c
AIEBI <124865658+AIEBI@users.noreply.github.com>
2025-03-18 08:25:21 +0530

    second commit

---
+++ Object Repository/Page_Equinox Constructions/a_Equinox Constructions.rs
@@ -0,0 +1,146 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Equinox Constructions</name>
+   <tag></tag>
+   <elementGuidId>5c50e3b2-880e-45f1-9ebb-2df01960aecb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>h2.wp-block-site-title > a</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//a[contains(text(),'Equinox Constructions')])[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=heading[name=&quot;Equinox Constructions&quot;i] >> internal:role=link</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>ccc3c181-f878-4bb1-aa73-64b3db686c26</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>http://3.91.5.249/wordpress</value>
+      <webElementGuid>f013486c-b286-482a-a2bb-0e8c5aee88d0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>target</name>
+      <type>Main</type>
+      <value>_self</value>
+      <webElementGuid>69b7b63f-45d7-473e-ac86-1713abbb84e7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>rel</name>
+      <type>Main</type>
+      <value>home</value>
+      <webElementGuid>a3c50861-eb8e-4d4c-8c1e-60d59cc1a8b6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-current</name>
+      <type>Main</type>
+      <value>page</value>
+      <webElementGuid>f6dc83d0-a559-4305-9ca7-17bace53719b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Equinox Constructions</value>
+      <webElementGuid>7547814f-4c5a-4b52-99a2-7de0a226e94a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;home blog wp-embed-responsive&quot;]/div[@class=&quot;wp-site-blocks&quot;]/footer[@class=&quot;wp-block-template-part&quot;]/div[@class=&quot;wp-block-group has-global-padding is-layout-constrained wp-block-group-is-layout-constrained&quot;]/div[@class=&quot;wp-block-group alignwide is-layout-flow wp-block-group-is-layout-flow&quot;]/div[@class=&quot;wp-block-group alignfull is-content-justification-space-between is-layout-flex wp-container-core-group-is-layout-10 wp-block-group-is-layout-flex&quot;]/div[@class=&quot;wp-block-columns is-layout-flex wp-container-core-columns-is-layout-1 wp-block-columns-is-layout-flex&quot;]/div[@class=&quot;wp-block-column is-layout-flow wp-block-column-is-layout-flow&quot;]/h2[@class=&quot;wp-block-site-title&quot;]/a[1]</value>
+      <webElementGuid>e85a571d-33e7-4f8a-88b9-6dd7699c6d6c</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>(//a[contains(text(),'Equinox Constructions')])[2]</value>
+      <webElementGuid>24408a90-3a46-4098-afe5-1a97fc9fe348</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='March 18, 2025'])[1]/following::a[1]</value>
+      <webElementGuid>e3ce676b-439d-4203-9cff-b18694e03c12</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Hello Peps!'])[1]/following::a[2]</value>
+      <webElementGuid>1652fbdc-509a-4263-bce5-dc62a320657f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Blog'])[2]/preceding::a[1]</value>
+      <webElementGuid>ef213745-952d-474c-aa27-b37dcdf50f16</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='About'])[1]/preceding::a[2]</value>
+      <webElementGuid>bf98011e-3f82-456c-8423-1618074c007f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>(//a[@href='http://3.91.5.249/wordpress'])[2]</value>
+      <webElementGuid>35e5c014-2f08-469c-8eba-bccdf47cecf1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div/h2/a</value>
+      <webElementGuid>359d00f8-c637-45fe-861f-f4dd8f54ad01</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'http://3.91.5.249/wordpress' and (text() = 'Equinox Constructions' or . = 'Equinox Constructions')]</value>
+      <webElementGuid>ef7738da-2716-4479-a753-d74b785fed8e</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5664621c-93ac-4ca1-86d9-de95affbca71</testSuiteGuid>
+   <testCaseLink>
+      <guid>a08a744e-3546-43b2-9ff5-8a2363f761c0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
